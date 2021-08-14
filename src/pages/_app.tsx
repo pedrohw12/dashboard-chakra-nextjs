bcdf4819040cea0b1e197beb0b6f87f53ba6c374
Pedro Wanderley <peu-zico@hotmail.com>
2021-08-14 15:37:29 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+import { QueryClientProvider } from 'react-query';
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services/mirage'
+import { queryClient } from '../services/queryClient';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+      s
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
+
+export default App
